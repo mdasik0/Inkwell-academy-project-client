77997ba08c972e0c79bc968d18db476ca7da4111
mdasik0 <asikthe1st@gmail.com>
2023-06-08 13:01:00 +0600

    Navbar Done

--- src/Component/Shared/Footer/Footer.jsx
+++ src/Component/Shared/Footer/Footer.jsx
@@ -1,10 +1,47 @@
-
 const Footer = () => {
-    return (
-        <div>
-            this is footer
+    // TODO: switch logo img with links
+  return (
+    <>
+    
+      <footer className="p-10 bg-base-200 text-base-content">
+        <div className=" mx-auto flex md:flex-row flex-col items-start md:items-center justify-around footer md:w-[1280px]">
+          <div>
+            <img className="w-20 h-20 object-cover" src="/favicon.png" alt="" />
+            <p>
+              <span className="logo_font text-lg">ArtVenture</span> Ltd.
+              <br />
+              Address: 19/B, Road 3, Dhanmondi, <br />
+              Dhaka-1205, Bangladesh.
+            </p>
+          </div>
+          <div>
+            <span className="footer-title">Services</span>
+            <a className="link link-hover">Art Critics</a>
+            <a className="link link-hover">LifeLong Support</a>
+            <a className="link link-hover">Friendly Instructors</a>
+            <a className="link link-hover">Learning Enviroment</a>
+          </div>
+          <div>
+            <span className="footer-title">Company</span>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Hiring Instructors</a>
+            <a className="link link-hover">sponsored by OhioArts.</a>
+          </div>
+          <div>
+            <span className="footer-title">Legal</span>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+            <a className="link link-hover"></a>
+          </div>
         </div>
-    );
+      </footer>
+      <div className="text-center text-xs py-4   bg-base-200 text-gray-500">
+        <p>Copyright © 2023 - All right reserved by <span className="logo_font text-lg">ArtVenture</span> Ltd.</p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/Component/Shared/Navbar/Navbar.jsx
+++ src/Component/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,89 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return (
-        <div>
-            this is navbar
+  const Navbar = (
+    <>
+      <li>
+        <Link>
+            home
+        </Link>
+      </li>
+      
+      <li>
+        <Link>
+            classes
+        </Link>
+      </li>
+      
+      <li>
+        <Link>
+            instructors
+        </Link>
+      </li>
+      
+      <li>
+        <Link>
+            sign in
+        </Link>
+      </li>
+      
+      <li>
+        <Link>
+            sign up
+        </Link>
+      </li>
+      
+    </>
+  );
+  return (
+    <div className="navbar mx-auto md:w-[1280px] md:bg-transparent bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content uppercase mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {/* navbar here */}
+            {Navbar}
+          </ul>
         </div>
-    );
+        {/* TODO: Update the Logo to a link before submit */}
+        {/* Logo Here */}
+        <div className="flex items-center justify-center gap-2">
+          <img className="h-16 md:block hidden" src="/favicon.png" alt="" />
+          <h4 className="text-2xl logo_font">ArtVenture</h4>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu uppercase font-semibold menu-horizontal px-1">{/* navbar here */}
+        {Navbar}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar online">
+          <div className="md:w-14 w-12  rounded-full">
+            <img src="https://www.shareicon.net/data/512x512/2016/09/01/822711_user_512x512.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
