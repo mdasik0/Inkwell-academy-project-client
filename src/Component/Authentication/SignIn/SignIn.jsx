991d6e8b529ca80054113f62510527ca46a54464
mdasik0 <asikthe1st@gmail.com>
2023-06-08 19:39:02 +0600

    sign up done sign in done

--- src/Component/Authentication/SignIn/SignIn.jsx
+++ src/Component/Authentication/SignIn/SignIn.jsx
@@ -1,34 +1,78 @@
 import { FaEye, FaEyeSlash, FaRegEnvelope, FaUnlock } from "react-icons/fa";
 import SocialLogin from "../SocialLogin/SocialLogin";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+
+
 
 const SignIn = () => {
 
-    const [show,setShow] = useState(false)
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email,password)
-        form.reset()
-    }
+  const {signInUser} = useContext(AuthContext)
+
+  const [show, setShow] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location.state?.from?.pathname || "/";
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // navigate
+    
+
+
+    // login
+    signInUser(email, password)
+      .then((result) => {
+        if (result.user) {
+          Swal.fire(
+            "Login Successful",
+            "You have logged in to your account successfully!",
+            "success"
+          );
+
+          navigate(from, { replace: true });
+        }
+      })
+      .catch((error) => console.error(error));
+
+    form.reset();
+  };
   return (
     <div className="border-2 mt-2 mb-10 p-10 mx-auto rounded-xl flex md:flex-row flex-col items-center justify-around md:w-[1280px]">
-        <div>
-            <img className="h-96" src="https://img.freepik.com/free-vector/login-concept-illustration_114360-757.jpg?w=740&t=st=1686214938~exp=1686215538~hmac=8f847e7428adfd8b2e527d680923c9a321f78e98f179c416b48e8c173487ee33" alt="" />
-        </div>
-      <form onSubmit={handleSubmit} className="flex flex-col relative p-10 border-4 border-blue-600 rounded-xl items-center ">
-        <h3 className="text-3xl font-bold text-blue-500 absolute left-10 -top-5 bg-white px-2 border-x-[3px] rounded border-blue-500">Sign In</h3>
+      <div>
+        <img
+          className="h-96"
+          src="https://img.freepik.com/free-vector/login-concept-illustration_114360-757.jpg?w=740&t=st=1686214938~exp=1686215538~hmac=8f847e7428adfd8b2e527d680923c9a321f78e98f179c416b48e8c173487ee33"
+          alt=""
+        />
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col relative p-10 border-4 border-blue-600 rounded-xl items-center "
+      >
+        <h3 className="text-3xl font-bold text-blue-500 absolute left-10 -top-5 bg-white px-2 border-x-[3px] rounded border-blue-500">
+          Sign In
+        </h3>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Your Email</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaRegEnvelope></FaRegEnvelope></span>
+            <span className="bg-blue-500 text-white">
+              <FaRegEnvelope></FaRegEnvelope>
+            </span>
             <input
-            name="email"
+              name="email"
+              required
               type="email"
               placeholder="Your email here"
               className="input border-2  border-blue-500"
@@ -40,31 +84,51 @@ const SignIn = () => {
             <span className="label-text">Your Password</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaUnlock></FaUnlock></span>
+            <span className="bg-blue-500 text-white">
+              <FaUnlock></FaUnlock>
+            </span>
             <input
-            name="password"
+              name="password"
+              required
               type={show ? `text` : `password`}
               placeholder="Your Password here"
               className="input border-2 border-blue-500"
             />
           </label>
-          {
-            show 
-            ? <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEye></FaEye></span> 
-            : <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEyeSlash></FaEyeSlash></span>
-          }
-          
+          {show ? (
+            <span
+              onClick={() => setShow(!show)}
+              className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+            >
+              <FaEye></FaEye>
+            </span>
+          ) : (
+            <span
+              onClick={() => setShow(!show)}
+              className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+            >
+              <FaEyeSlash></FaEyeSlash>
+            </span>
+          )}
         </div>
         <div className="w-full ">
-        <label className="label">
-            <span className="label-text hover:text-blue-700 hover:underline font-semibold">Forgotten Password?</span>
+          <label className="label">
+            <span className="label-text hover:text-blue-700 hover:underline font-semibold">
+              Forgotten Password?
+            </span>
           </label>
-            <input className="uppercase text-sm btn bg-blue-600 text-white hover:bg-blue-400" type="submit" value="Sign In" />
+          <input
+            className="uppercase text-sm btn bg-blue-600 text-white hover:bg-blue-400"
+            type="submit"
+            value="Sign In"
+          />
         </div>
-        <Link to={'/signUp'}>
-        <label className="label">
-            <span className="label-text hover:text-blue-700 hover:underline font-semibold">New Here? Create an account</span>
-        </label>
+        <Link to={"/signUp"}>
+          <label className="label">
+            <span className="label-text hover:text-blue-700 hover:underline font-semibold">
+              New Here? Create an account
+            </span>
+          </label>
         </Link>
         <div className="divider text-sm font-semibold">or</div>
         <SocialLogin></SocialLogin>
--- src/Component/Authentication/SignUp/SignUp.jsx
+++ src/Component/Authentication/SignUp/SignUp.jsx
@@ -1,111 +1,305 @@
-import { FaAddressCard, FaCameraRetro, FaEye, FaEyeSlash, FaLock, FaRegEnvelope, FaUnlock } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import {
+  FaAddressCard,
+  FaCameraRetro,
+  FaExclamationCircle,
+  FaEye,
+  FaEyeSlash,
+  FaLock,
+  FaRegEnvelope,
+  FaUnlock,
+} from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../SocialLogin/SocialLogin";
-import { useState } from "react";
+import { useContext, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
-    const [show,setShow] = useState(false)
+  // ================= States ================
+  const { createUser, updateUser } = useContext(AuthContext);
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState('')
+  console.log(error.message)
+  const navigate = useNavigate();
 
-    return (
-        <div className="border-2 mt-2 mb-10 p-10 mx-auto rounded-xl flex md:flex-row flex-col items-center justify-around md:w-[1280px]">
-        <div>
-            <img className="h-96" src="https://img.freepik.com/free-vector/login-concept-illustration_114360-757.jpg?w=740&t=st=1686214938~exp=1686215538~hmac=8f847e7428adfd8b2e527d680923c9a321f78e98f179c416b48e8c173487ee33" alt="" />
-        </div>
-      <form className="flex flex-col relative p-10 border-4 border-blue-600 rounded-xl items-center ">
-        <h3 className="text-3xl font-bold text-blue-500 absolute left-10 -top-5 bg-white px-2 border-x-[3px] rounded border-blue-500">Sign Up</h3>
+  const {
+    register,
+    reset,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
+
+  // ================ On Submit ===============
+  const onSubmit = (data) => {
+    const email = data.email;
+    const password = data.password;
+    const name = data.name;
+    const photoUrl = data.photoUrl;
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+
+        updateUser(name, photoUrl)
+          .then(() => {
+            const SavedUser = { name: name, email: email };
+            fetch(`http://localhost:5000/users`, {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(SavedUser),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                if (data.insertedId) {
+                  Swal.fire(
+                    "Welcome!",
+                    "Your Account Has been Created!",
+                    "success"
+                  );
+                  reset();
+                  navigate("/");
+                }
+              });
+          })
+          .catch((error) => setError(error));
+      })
+      .catch((error) => setError(error));
+  };
+
+  return (
+    <div className="border-2 mt-2 mb-10 md:p-10 p-2 mx-auto rounded-xl flex md:flex-row flex-col items-center justify-around md:w-[1280px]">
+      <div>
+        <img
+          className="w-full"
+          src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?w=740&t=st=1686217312~exp=1686217912~hmac=1472edd69e4777028b321fd0f0f8abe5382b5382446b5a010b939b84c6df0fef"
+          alt=""
+        />
+      </div>
+
+      {/* form start here */}
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex md:w-1/2 w-full  flex-col relative md:p-10 p-4 border-4 border-blue-600 rounded-xl items-center "
+      >
+        <h3 className="text-3xl font-bold text-blue-500 absolute left-10 -top-5 bg-white px-2 border-x-[3px] rounded border-blue-500">
+          Sign Up
+        </h3>
+
+        {/* name */}
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaAddressCard></FaAddressCard></span>
+            <span className="bg-blue-500 text-white">
+              <FaAddressCard></FaAddressCard>
+            </span>
             <input
-                name='name'
+              name="name"
+              {...register("name")}
               type="text"
               placeholder="Your FullName here"
               className="input border-2  border-blue-500"
             />
           </label>
         </div>
+
+        {/* Photo Url */}
+
         <div className="form-control">
           <label className="label">
             <span className="label-text">PhotoUrl</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaCameraRetro></FaCameraRetro></span>
+            <span className="bg-blue-500 text-white">
+              <FaCameraRetro></FaCameraRetro>
+            </span>
             <input
-                name='photoUrl'
+              name="photoUrl"
+              {...register("photoUrl")}
               type="text"
               placeholder="Your PhotoUrl here"
               className="input border-2  border-blue-500"
             />
           </label>
         </div>
+
+        {/* email */}
+
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaRegEnvelope></FaRegEnvelope></span>
+            <span className="bg-blue-500 text-white">
+              <FaRegEnvelope></FaRegEnvelope>
+            </span>
             <input
-                name='email'
+              {...register("email", { required: true })}
               type="email"
               placeholder="Your Email here"
               className="input border-2  border-blue-500"
             />
           </label>
         </div>
+            {/* email validation */}
+        {errors.email && (
+          <span className="text-red-500 text-xs font-semibold flex items-center gap-1 mt-2">
+            {" "}
+            <FaExclamationCircle></FaExclamationCircle> Email is required
+          </span>
+        )}
+
+        {/* Password */}
+
         <div className="form-control relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
+
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaUnlock></FaUnlock></span>
+            <span className="bg-blue-500 text-white">
+              <FaUnlock></FaUnlock>
+            </span>
+
+            {/* input box */}
             <input
-                name='password'
-              type={show ? `text` : `password`} 
+              {...register("password", {
+                required: true,
+                minLength: 6,
+                maxLength: 20,
+                pattern: /(?=.*?[A-Z])(?=.*?[#?!@$%^&*-])/,
+              })}
+              type={show ? `text` : `password`}
               placeholder="New Password"
               className="input border-2 border-blue-500"
             />
+            {/* input ends */}
+            {/* eye btn */}
+            {show ? (
+              <span
+                onClick={() => setShow(!show)}
+                className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+              >
+                <FaEye></FaEye>
+              </span>
+            ) : (
+              <span
+                onClick={() => setShow(!show)}
+                className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+              >
+                <FaEyeSlash></FaEyeSlash>
+              </span>
+            )}
           </label>
-          {
-            show 
-            ? <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEye></FaEye></span> 
-            : <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEyeSlash></FaEyeSlash></span>
-          }
         </div>
+        {/* error */}
+        {errors.password && (
+          <span className="text-red-500 text-xs font-semibold flex items-center gap-1 mt-2">
+            {" "}
+            <FaExclamationCircle></FaExclamationCircle> Password is required
+          </span>
+        )}
+        {errors.password?.type === "minLength" && (
+          <span className="text-red-500 text-xs font-semibold flex items-center gap-1 mt-2">
+            {" "}
+            <FaExclamationCircle></FaExclamationCircle> Password is too short, <br />
+            Atleast give 6 characters
+          </span>
+        )}
+        {errors.password?.type === "maxLength" && (
+          <span className="text-red-500 text-xs font-semibold flex items-center gap-1 mt-2">
+            {" "}
+            <FaExclamationCircle></FaExclamationCircle> Password is too Long, <br />
+            give less then 20 characters
+          </span>
+        )}
+        {errors.password?.type === "pattern" && (
+          <span className="text-red-500 text-xs font-semibold flex items-center gap-1 mt-2">
+            {" "}
+            <FaExclamationCircle></FaExclamationCircle> Make sure your Password
+            includes: <br />   1 Capital letter, 1 Special character
+          </span>
+        )}
+        {/* error ends here */}
+
+        {/* Confirm Password */}
+
         <div className="form-control relative">
           <label className="label">
             <span className="label-text">Confirm Password</span>
           </label>
           <label className="input-group">
-            <span className="bg-blue-500 text-white"><FaLock></FaLock></span>
+            <span className="bg-blue-500 text-white">
+              <FaLock></FaLock>
+            </span>
             <input
-                name='confirmPassword'
-              type={show ? `text` : `password`} 
+              {...register("confirmPassword", {
+                validate: (value) =>
+                  value === password.current || "Passwords do not match",
+              })}
+              type={show ? `text` : `password`}
               placeholder="Confirm Password"
               className="input border-2 border-blue-500"
             />
           </label>
-          {
-            show 
-            ? <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEye></FaEye></span> 
-            : <span onClick={() => setShow(!show)} className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"><FaEyeSlash></FaEyeSlash></span>
-          }
+          {show ? (
+            <span
+              onClick={() => setShow(!show)}
+              className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+            >
+              <FaEye></FaEye>
+            </span>
+          ) : (
+            <span
+              onClick={() => setShow(!show)}
+              className="absolute bottom-2 duration-500 text-blue-700 rounded-full p-2 right-1"
+            >
+              <FaEyeSlash></FaEyeSlash>
+            </span>
+          )}
         </div>
-        <div className="w-full my-6">
-            <input className="uppercase text-sm btn bg-blue-600 text-white hover:bg-blue-400" type="submit" value="Sign Up" />
+        {errors.confirmPassword && (
+          <span className="text-red-500 text-sm mt-2 flex items-center font-semibold"> <FaExclamationCircle></FaExclamationCircle> {errors.confirmPassword.message}</span>
+        )}
+        <div>
+          <h4 className="text-xs font-semibold text-red-500">{error.message}</h4>
         </div>
-        <Link to={'/signIn'}>
-        <label className="label">
-            <span className="label-text hover:text-blue-700 hover:underline font-semibold">Already have an account?</span>
-        </label>
+        
+
+        {/* Sign Up btn */}
+
+        <div className="my-6">
+          <input
+            className="uppercase text-sm btn bg-blue-600 text-white hover:bg-blue-400"
+            type="submit"
+            value="Sign Up"
+          />
+        </div>
+
+        {/* link to signIN */}
+
+        <Link to={"/signIn"}>
+          <label className="label">
+            <span className="label-text hover:text-blue-700 hover:underline font-semibold">
+              Already have an account?
+            </span>
+          </label>
         </Link>
+
         <div className="divider text-sm font-semibold">or</div>
         <SocialLogin></SocialLogin>
       </form>
     </div>
-    );
+  );
 };
 
 export default SignUp;
