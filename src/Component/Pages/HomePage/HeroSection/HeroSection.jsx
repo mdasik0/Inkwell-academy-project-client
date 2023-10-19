c52b34999d79d251e7517cc64cfb203435bfa748
mdasik0 <asikthe1st@gmail.com>
2023-10-19 23:26:48 +0600

    responsive hero section update done

--- src/Component/Pages/HomePage/HeroSection/HeroSection.jsx
+++ src/Component/Pages/HomePage/HeroSection/HeroSection.jsx
@@ -1,11 +1,11 @@
 import "./HeroSection.css";
 const HeroSection = () => {
   return (
-    <div className="text-white w-full h-[96vh] flex justify-center flex-col p-36  HeroSectionBG">
+    <div className="text-white w-full h-[96vh] flex justify-center flex-col lg:p-36 p-6 py-10  HeroSectionBG">
       <h4 className="text-sm  uppercase">
         Welcome to Inkwell Academy
       </h4>
-      <h2 className="text-4xl leading-[1.3] w-[500px] font-bold mt-2">Best Online Drawing Course Website</h2>
+      <h2 className="text-4xl leading-[1.3] w-fit md:w-[500px] font-bold mt-2">Best Online Drawing Course Website</h2>
       <h3 className="leading-6 text-sm mt-5">Our website Guides new ArtStudents From the starting day <br /> With The help of our guiding course. You can start drawing at any age!!!</h3>
 
       <div className="flex gap-4 mt-6">
