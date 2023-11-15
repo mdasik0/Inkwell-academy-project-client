47bb1c94afe425310c897417cea0fff0fad76def
mdasik0 <asikthe1st@gmail.com>
2023-11-16 00:07:28 +0600

    onclick input text delete

--- src/Component/Pages/Classes/Classes.jsx
+++ src/Component/Pages/Classes/Classes.jsx
@@ -1,14 +1,35 @@
 import { FaChair, FaDollarSign } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import { LuSearch } from "react-icons/lu";
 import useUserRole from "../../../Hooks/useUserRole";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Helmet } from "react-helmet-async";
 import useEnroll from "../../../Hooks/useEnroll";
-import "./Classes.css"
+import "./Classes.css";
 
 const Classes = () => {
   const [classes, setclasses] = useState([]);
+  const [InputText, setInputText] = useState(false);
+  const [clear, setClear] = useState(false);
   const { handleSelectedClass } = useEnroll();
+
+  const inputValue = useRef();
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    const todoInput = e.target.value;
+    if (todoInput) {
+      setInputText(true);
+    } else {
+      setInputText(false);
+    }
+  };
+  if (clear) {
+    inputValue.current.value = "";
+    setClear(false);
+    setInputText(false);
+  }
 
   useEffect(() => {
     axios
@@ -31,6 +52,29 @@ const Classes = () => {
       <Helmet>
         <title>Inkwell | Classes</title>
       </Helmet>
+      <div className="bg-yellow-400 h-[150px] flex justify-center items-center">
+        <form>
+          <div className="bg-white px-3 py-2 border-2 flex items-center gap-2 rounded w-[500px] border-[#1eb2a6]">
+            <LuSearch className="text-xl text-[#1eb2a6]" />
+            <input
+              type="text"
+              onChange={handleInput}
+              ref={inputValue}
+              placeholder={`Enter Course Name to search`}
+              className="outline-none w-full bg-transparent"
+            />
+            {InputText ? (
+              <RxCross2
+                onClick={() => setClear(true)}
+                title="Clear"
+                className={`text-xl text-red-600 cursor-pointer duration-200 hover:text-red-700`}
+              />
+            ) : (
+              ""
+            )}
+          </div>
+        </form>
+      </div>
 
       <div className="md:p-10 p-3 grid md:grid-cols-3 grid-cols-1 gap-10">
         {/* card  */}
@@ -40,7 +84,7 @@ const Classes = () => {
             className={
               data.seats <= 0
                 ? `pointer-events-none h-[500px] rounded-xl shadowCustomClass w-full md:w-[350px]  bg-red-600`
-                : `h-[500px] rounded-2xl shadowCustomClass w-full md:w-[350px] bg-white`
+                : `h-[500px] rounded-2xl cursor-pointer hover:scale-105 hover:bg-[#54dfd3]  duration-500 shadowCustomClass w-full md:w-[350px] bg-white`
             }
           >
             <div className="h-[250px]">
@@ -53,41 +97,33 @@ const Classes = () => {
             <div className="h-[230px] flex flex-col justify-between px-4 w-full">
               <div className="flex items-center justify-between">
                 <div>
-                  <h1 className="text-xl font-semibold">{data.className}</h1>
-                  <h4 className="text-sm font-semibold text-slate-400">
-                    {data.name}
-                  </h4>
+                  <h1 className="text-2xl Bebas_font">{data.className}</h1>
+                  <h4 className="text-sm text-stone-600">By {data.name}</h4>
                 </div>
               </div>
 
-              <div>
-                <div className="flex flex-col items-start justify-evenly">
+              <>
+                <div className="flex flex-col font-medium items-start justify-evenly">
                   <div className="flex gap-2 items-center  rounded-full py-1 text-center">
                     <FaChair></FaChair>
-                    <h3 className="text-xs text-slate-500 font-semibold">
-                      Total Seats:
-                    </h3>
-                    <h2 className="text-xs font-semibold">{data.Totalseats}</h2>
+                    <h3 className="text-sm text-stone-600">Total Seats:</h3>
+                    <h2 className="text-sm">{data.Totalseats}</h2>
                   </div>
                   <div className="flex gap-2 items-center  rounded-full py-1 text-center">
                     <FaChair></FaChair>
-                    <h3 className="text-xs text-slate-500 font-semibold">
-                      Available Seats:
-                    </h3>
-                    <h2 className="text-xs font-semibold">{data.seats}</h2>
+                    <h3 className="text-sm text-stone-600">Available Seats:</h3>
+                    <h2 className="text-sm">{data.seats}</h2>
                   </div>
                   <div className="flex gap-2 items-center rounded-full py-1 text-center">
                     <FaDollarSign></FaDollarSign>
-                    <h3 className="text-xs text-slate-500 font-semibold">
-                      Price:
-                    </h3>
-                    <h2 className="text-xs font-semibold">$ {data.price}</h2>
+                    <h3 className="text-sm text-stone-600">Price:</h3>
+                    <h2 className="text-sm">$ {data.price}</h2>
                   </div>
                 </div>
                 <div className="mt-2">
                   <div className="flex gap-1">
-                    <h4 className="text-xs text-slate-400 ">Days Left:</h4>
-                    <h5 className="text-xs font-semibold">1 Day</h5>
+                    <h4 className="text-sm text-stone-600">Days Left:</h4>
+                    <h5 className="text-sm">1 Day</h5>
                   </div>
                 </div>
                 <button
@@ -102,7 +138,7 @@ const Classes = () => {
                 >
                   Enroll
                 </button>
-              </div>
+              </>
             </div>
           </div>
         ))}
